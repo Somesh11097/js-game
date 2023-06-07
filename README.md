4d7ec1d192f79c89c6de45d1f03fe7701fc0f880
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2023-06-07 11:53:19 +0530

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# whack-a-mole-game
+You can run directly in your system by using any IDE.
+
+
+![p1](https://github.com/Shivansahni/whack-a-mole-game/assets/56557700/0c58de48-17e6-42bd-a261-ad96c84b9ecc)
+![p2](https://github.com/Shivansahni/whack-a-mole-game/assets/56557700/485c145e-75b5-4833-a5ea-8836e7cfe612)
+![p3](https://github.com/Shivansahni/whack-a-mole-game/assets/56557700/a27273bb-4970-47ae-92fa-7e1bc9171bd9)
---
+++ whack.js
@@ -0,0 +1,96 @@
+let scoreEl = document.getElementById('score');
+let timeLeftEl = document.getElementById('timeLeft');
+let startNewGameButton = document.getElementById('startNewGame');
+let pauseGameButton = document.getElementById('pauseGame');
+let squares = document.querySelectorAll('.square');
+let grid =  document.getElementsByClassName('grid')[0];
+let gameMusic = new Audio('./assests/gameMusic.mp3');
+let hitMusic = new Audio('./assests/hitMusic.mp3');
+
+let score=0;
+let timeLeft=0;
+let hitPosition= null;
+let timerId = null;
+let randomMoleId = null;
+
+//randomly place mole
+function randomMole(){
+    squares.forEach(square => {
+        square.classList.remove("mole");
+    })
+
+    let randomSqaure = squares[Math.floor(Math.random()*squares.length)];
+    randomSqaure.classList.add('mole')
+    hitPosition = randomSqaure.id;
+}
+
+function countDown(){
+    timeLeft--;
+    timeLeftEl.innerHTML = `Time Left: ${timeLeft}`;
+    if(timeLeft===0)
+    {
+        gameMusic.pause();
+        clearInterval(timerId);
+        clearInterval(randomMoleId);
+        gameMusic.currentTime = 0;
+        grid.style.display="none";
+        pauseGameButton.style.display="none";   
+        timeLeftEl.style.display="none";
+    }
+}
+randomMole();
+
+function startGame(){
+    score=0;
+    timeLeft=60;
+    scoreEl.innerHTML = "Your score: 0";
+    timeLeft.innerHTML = "Time Left: 60"; 
+    pauseGameButton.style.display="inline";
+    grid.style.display="grid";
+    pauseGameButton.innerHTML='Pause';
+    window.scrollTo(0, document.body.scrollHeight);
+    gameMusic.play();
+    //callback function
+    //setinterval call function at regular interval
+  timerId =  setInterval(randomMole,1000);
+randomMoleId = setInterval(countDown,1000);
+}
+
+function pauseResumeGame(){
+    if(pauseGameButton.textContent === 'Pause'){
+        gameMusic.pause();
+        clearInterval(timerId);
+        clearInterval(randomMoleId);
+        timerId= null;
+        randomMoleId=null;
+pauseGameButton.textContent = 'Resume';
+}
+else{
+    gameMusic.play();
+    timerId =  setInterval(randomMole,1000);
+    randomMoleId = setInterval(countDown,1000);
+    pauseGameButton.textContent = 'Pause';
+}
+}
+
+
+
+squares.forEach(square =>{
+    square.addEventListener('mousedown',()=> {
+        if(timerId !== null)
+        {
+        if(square.id === hitPosition)
+        {
+            hitMusic.play();
+            setTimeout(()=> {
+                hitMusic.pause()
+            },1000);
+            score++;
+            scoreEl.innerHTML = `Your score: ${score}`;
+            hitPosition = null;
+                }
+                    }
+    });
+})
+startNewGameButton.addEventListener('click',startGame);
+pauseGameButton.addEventListener('click', pauseResumeGame);
